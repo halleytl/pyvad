bce7a23dab5f84441ccba962190e1e432bf836dc
Halleytl <hl454943442@qq.com>
2014-12-26 10:07:07 +0800

    v1.1

--- example/test_record.py
+++ example/test_record.py
@@ -2,39 +2,7 @@
 #-*- coding: utf-8 -*-
 import sys 
 sys.path.append("../")
-from audio import Audio
-from vad import Vad 
-import threading
-
-class StreamParser(Vad):
-    def __init__(self):
-        self.record = Audio(chuck=256)
-        self.active = False
-        self.play =Audio()
-        Vad.__init__(self)
-   
-    def start_mic(self):
-        print "start recording"
-        t = threading.Thread(target=self.mic_record)
-        t.setDaemon(True)
-        t.start()
-
-    def mic_record(self):
-        self.record.record_stream_start()
-        self.active = True
-        print "The microphone has opened"
-        while self.active: 
-            data = self.record.record_read()
-            self.cache_frames.append(data)
-        self.record.record_stream_end()
-
-    def close_mic(self):
-        print "close_mic() enable"
-        if self.record:
-            self.active = False
-
-    def play_stream(self, data):
-        self.play.play_stream(data)
+from RecordParser import StreamParser
 
 if __name__ == "__main__":
     
