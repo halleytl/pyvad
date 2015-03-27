5d69b14b7683f39f6fd24f41e518b572ec0136f0
406384832@qq.com <lei@ubuntu.ubuntu-domain>
2015-03-27 22:12:21 +0800

    程序优化

--- RecordParser.py
+++ RecordParser.py
@@ -13,7 +13,7 @@ class StreamParser(Vad):
         self.play =Audio()
         Vad.__init__(self)
    
-    def start_mic(self):
+    def open_mic(self):
         print "start recording"
         t = threading.Thread(target=self.mic_record)
         t.setDaemon(True)
@@ -27,20 +27,42 @@ class StreamParser(Vad):
             data = self.record.record_read()
             self.cache_frames.append(data)
         self.record.record_stream_end()
+        print "exit mic"
+         
 
     def close_mic(self):
-        print "close_mic() enable"
+        print "stop recording"
         if self.record:
+            print self.active
             self.active = False
+       
 
     def play_stream(self, data):
         self.play.play_stream(data)
 
+def test():
+    stream_test = StreamParser()
+    stream_test.open_mic()
+    import time
+    time.sleep(5)
+    stream_test.close_mic()
+    time.sleep(5)
+    import util
+    data = "".join(stream_test.cache_frames)      
+    util.save_file(data)
+    stream_test.play_stream(data)
+
+def main():
+    stream_test = StreamParser()
+    stream_test.open_mic()
+    import time
+    while 1:
+        
+        time.sleep(5)
+
 if __name__ == "__main__":
+    main()
     
-   stream_test = StreamParser()
-   stream_test.start_mic()
-   stream_test.run(fun=stream_test.play_stream)
 
 
 
--- vad.py
+++ vad.py
@@ -7,9 +7,8 @@ import threading
 from util import save_file
 
 #需要添加录音互斥功能能,某些功能开启的时候录音暂时关闭
-def ZCR(wave_data):
+def ZCR(curFrame):
     #过零率
-    #curFrame = wave_data[np.arange(0, len(wave_data))]
     tmp1 = curFrame[:-1]
     tmp2 = curFrame[1:]
     sings = (tmp1*tmp2<=0)
@@ -17,28 +16,28 @@ def ZCR(wave_data):
     zcr = np.sum(sings*diffs)
     return zcr
 
-def STE(wave_data):
+def STE(curFrame):
     #短时能量
-    #curFrame = wave_data[np.arange(0, len(wave_data))]
     amp = np.sum(np.abs(curFrame))
     return amp
 
 class Vad(object):
-    def __init__(self): #初始短时能量高门限
+    def __init__(self): 
+        #初始短时能量高门限
         self.amp1 = 140
         #初始短时能量低门限
         self.amp2 = 120
         #初始短时过零率高门限
         self.zcr1 = 10
         #初始短时过零率低门限
-        self.zcr2 = 5
+        self.zcr2 = 5 
         #允许最大静音长度
         self.maxsilence = 100
         #语音的最短长度
         self.minlen = 40
         #偏移值
         self.offsets = 40
-        self.offsete = 200
+        self.offsete = 40
         #能量最大值
         self.max_en = 20000
         #初始状态为静音
@@ -46,7 +45,6 @@ class Vad(object):
         self.count = 0
         self.silence = 0
         self.frame_len = 256
-        #self.frame_inc = 80
         self.frame_inc = 128
         self.cur_status = 0
         self.frames = []
@@ -60,10 +58,37 @@ class Vad(object):
         self.cache_frames = []
         #最大缓存长度
         self.cache_frames_num = 0
-        
         self.end_flag = False
         self.wait_flag = False
         self.on = True
+        self.callback = None
+    def set_amph(self, volume):
+        
+        if 90 < volume <= 100:
+            self.amp1 = 21000
+        elif 80 < volume <= 90:
+            self.amp1 = 19000
+        elif 70 < volume <= 80:
+            self.amp1 = 7000
+        elif 60 < volume <= 70:
+            self.amp1 = 2000
+        else:
+            self.amp1 = 600
+          
+    def clean(self):
+        self.frames = []
+        #数据开始偏移
+        self.frames_start = []
+        self.frames_start_num = 0
+        #数据结束偏移
+        self.frames_end = []
+        self.frames_end_num = 0
+        #缓存数据
+        self.cache_frames = []
+        #最大缓存长度
+        self.cache_frames_num = 0
+        self.end_flag = False
+        self.wait_flag = False
 
     def go(self):
         self.wait_flag = False
@@ -73,22 +98,27 @@ class Vad(object):
     def stop(self):
         self.on = False
 
-    def run(self, fun=None):
-        if fun is None: 
-            fun = save_file 
+    def run(self, sound=None):
         print "开始执行音频端点检测" 
         step = self.frame_len - self.frame_inc
         num = 0
-        while self.on:
+        if self.callback is None:
+            self.callback = save_file
+        while 1:
             #开始端点
             #获得音频文件数字信号
             if self.wait_flag:
                 sleep(1)
                 continue
             if len(self.cache_frames) <2:
-                print "当前缓存中无数据等待接收数据"
-                sleep(1)
+                sleep(0.05)
                 continue
+            if sound:
+                if sound.status:
+                    self.set_amph(sound.volume)
+                else:
+                    self.amp1 = 140
+
             record_stream = "".join(self.cache_frames[:2])
             wave_data = np.fromstring(record_stream, dtype=np.int16)
             wave_data = wave_data*1.0/self.max_en  
@@ -100,6 +130,7 @@ class Vad(object):
             amp = STE(data)**2
             #返回当前音频数据状态
             res = self.speech_status(amp, zcr)
+            #print res,
             num = num+1
             self.frames_start.append(speech_data)
             self.frames_start_num += 1
@@ -113,10 +144,12 @@ class Vad(object):
                  #下一段语音开始，或达到缓存阀值
                  if res == 2 or self.frames_end_num == self.offsete :
                      speech_stream =  b"".join(self.frames+self.frames_end)
-                     fun(speech_stream)
+                     self.callback(speech_stream)
                                
                      #数据环境初始化
+                     #self.clean()
                      self.end_flag = False
+                     
                      self.frames = []
                      self.frames_end_num = 0
                      self.frames_end = []
@@ -129,10 +162,11 @@ class Vad(object):
                 #添加当前的语音数据
                 self.frames.append(speech_data)
             if res == 3:
-               
+                  
                 self.frames.append(speech_data)
                 #开启音频结束标志
                 self.end_flag = True
+               
             self.cur_status = res
         return 0
 
