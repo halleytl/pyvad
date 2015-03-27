e9690f4b999fb6d27f531177ab86692dfa850290
“halleytl” <hl454943442@qq.com>
2015-03-27 22:14:11 +0800

    程序优化

--- example/test_record.py
+++ example/test_record.py
@@ -7,7 +7,7 @@ from RecordParser import StreamParser
 if __name__ == "__main__":
     
    stream_test = StreamParser()
-   stream_test.start_mic()
+   stream_test.open_mic()
    stream_test.run(fun=stream_test.play_stream)
 
 
